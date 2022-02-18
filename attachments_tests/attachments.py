2ac60dc3402dd03d696b6279e6501969a27d80d1
Matthew Kosack <kosackm@moravian.edu>
2022-02-18 15:23:47 -0500

    show todos

--- attachments_tests/attachments.py
+++ attachments_tests/attachments.py
@@ -1,8 +1,22 @@
 from dotenv import load_dotenv
+import requests
+import os
 
 def go():
     # Make sure to add a .env file with
     # KEY=<your_key>
+    load_dotenv()
+    key = os.getenv('KEY')
+
+    # TODO: get jsons? from mongo db comments
+
+    # TODO: check for attachments in those
+        # TODO: check if the attachments linked come in pairs or some are one link, etc.
+
+    # TODO: download the links from those attachments
+
+        # TODO: check the types of documents
+
     pass
 
 
