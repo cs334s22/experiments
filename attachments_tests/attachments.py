430cf78cfe78a8ec6280835e54aee54be155ce49
Isaac Wood <woodi@moravian.edu>
2022-02-18 15:36:57 -0500

    added more code

--- attachments_tests/attachments.py
+++ attachments_tests/attachments.py
@@ -16,6 +16,14 @@ def go():
     params = {'api-key':key}
 
     # TODO: get jsons? from mongo db comments
+    try:
+        def get_jsons():
+            for json in jsons:
+                pass
+
+    response = requests.get(url, params=params, timeout=.001)
+    response.raise_for_status()
+    get_jsons(response.json())
 
     # TODO: check for attachments in those
         # TODO: check if the attachments linked come in pairs or some are one link, etc.
