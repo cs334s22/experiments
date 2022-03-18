f42e412db3cb5be1bf883c9e5a864b14ac1b970d
Matthew Kosack <kosackm@moravian.edu>
2022-03-18 15:32:17 -0400

    modified get comments

--- attachments_tests/get_comments.py
+++ attachments_tests/get_comments.py
@@ -1,36 +1,29 @@
 import json
-# import pymongo
 from pymongo import MongoClient
-# from bson.json_util import dumps, loads
 
 uri = "mongodb://127.0.0.1:27017/"
 client = MongoClient(uri)
 
 db = client.mirrulations
 comments = db.comments
-documents = db.documents
-collections = db.list_collections()
-# cursor = comments.find().limit(100)
-cursor = documents.find().limit(100)
-total_count = len(list(cursor))
+total_count = len(list(comments.find({}, {"data.id": 1, "data.relationships.attachments.links.related" : 1})))
 
 
 def get_comments():
-    count = 0
-    with open('comments100.json', 'w') as file:
-        file.write('[')
-        # for comment in comments.find().limit(100):
-        for document in documents.find().limit(100):
-            count += 1
-            file.write(json.dumps(document['data']['relationships']['attachments']['links']['self'], default=str))
-            # file.write(json.dumps(comments['data']['relationships']['attachments']['links']['self'], default=str))
-            if count < total_count:
-                file.write(', \n')
-            # print(document)
-        # for collect in collections:
-        #     print(collect)
-        file.write(']')
+    # The limit 10 is just so we don't check everything in the database (for now)
+    for count, comment in enumerate(comments.find({}, 
+                            {"data.id": 1, "data.relationships.attachments.links.related" : 1}).limit(10)):
+        with open(f'comment{count}.json', 'w') as file:
+            # file.write('[')
+            # Ideally we will just be sending this json, for now we save to disk
+            file.write(json.dumps(comment, default=str))
+            # if count < total_count:
+            #     file.write(', \n')
+            # file.write(']')
 
 
 if __name__ == '__main__':
     get_comments()
+    # with open('comments10.json') as file:
+    #     json_file = json.load(file)
+    #     print(type(json_file))
