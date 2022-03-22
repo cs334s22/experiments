0ca9f10fc793d0a378b2420c3aa1360cf07a7068
Matthew Kosack <kosackm@moravian.edu>
2022-03-22 17:14:50 -0400

    add some better documentation

--- attachments_tests/get_comment_attachments.py
+++ attachments_tests/get_comment_attachments.py
@@ -1,5 +1,18 @@
-# You will need a folder named 'attachments' and one named 'comments'
-# You will also need a .env file containing 'KEY=<YOUR_KEY>'
+'''
+You will need a folder named 'comments', one named 'related_response', and one named 'downloads'
+You will also need a .env file containing 'KEY=<YOUR_KEY>' as this makes requests to regulations.gov
+
+Your virtual environment must also have:
+    PyMongo: pip install pymongo
+    dotenv: pip install python-dotenv
+
+Make sure to update your .gitignore to include:
+    comments
+    related_response
+    downloads
+
+Otherwise, you might accidentally upload all of the files that are created
+'''
 
 import json
 import os
@@ -20,6 +33,9 @@ params = { 'api_key': os.getenv('KEY') }
 
 
 def get_comments():
+    '''
+    Save the comments from the database, as jsons.
+    '''
     # The limit 10 is just so we don't check everything in the database
     for count, comment in enumerate(comments.find({}, 
                             {"data.id": 1, "data.relationships.attachments.links.related" : 1}).limit(10)):
@@ -30,7 +46,8 @@ def get_comments():
 
 def get_download_response(read_dir, write_dir):
     '''
-    
+    Makes a request to the related link from a comment. 
+    The response from this contains the links for the files to be downloaded.
     '''
     for filename in os.listdir(read_dir):
         related_file_path = os.path.join(read_dir, filename)
@@ -44,7 +61,10 @@ def get_download_response(read_dir, write_dir):
 
 def download_attachments(read_dir, write_dir):
     '''
-    Make requests to regulation.gov to download the file.
+    Make requests to regulation.gov to download the files from the the related responses.
+
+    NOTE: many of the files are the same document just in a different format. For example, there
+    may be a pdf and .doc file, but they are the same document. 
 
     Valid file types include: bmp, docx, gif, jpg, jpeg, pdf, png, pptx, rtf, sgml, tif, tiff, txt, wpd, xlsx, xml.
     See regulations.gov (click comment and above the file upload it shows valid file types)
@@ -53,9 +73,6 @@ def download_attachments(read_dir, write_dir):
         related_file_path = os.path.join(read_dir, filename)
         with open(related_file_path, 'r') as related_response:
             data = json.load(related_response)
-
-            # TODO: we need to test something that has multiple files (the tests here seem to only have one attachment, but multiple formats)
-
             downloads = data['data'][0]['attributes']['fileFormats']
             for i,download in enumerate(downloads):
                 urllib.request.urlretrieve(download['fileUrl'], f'{write_dir}/download_{filename[:-5]}_{i}.{download["format"]}')
@@ -64,7 +81,6 @@ def download_attachments(read_dir, write_dir):
                 # response = requests.get(download['fileUrl'])
                 # with open(f'{write_dir}/download_{filename[:-5]}_{i}.{download["format"]}', 'w') as download: 
                 #     download.write(response.content)
-
 
 
 if __name__ == '__main__':
