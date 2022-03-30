1962dd93ca182bea2dcca31dc8450e05697cf9bc
lizvicente <vicentee@moravian.edu>
2022-03-30 13:58:22 -0400

    import cyntax fix

--- download_jobs/work_generator.py
+++ download_jobs/work_generator.py
@@ -2,12 +2,10 @@ import os
 import time
 import dotenv
 import redis
-from mirrgen.search_iterator import SearchIterator
-from mirrgen.results_processor import ResultsProcessor
-from mirrcore.regulations_api import RegulationsAPI
-from mirrcore.job_queue import JobQueue
-from mirrcore.data_storage import DataStorage
-from mirrcore.redis_check import is_redis_available
+from search_iterator import SearchIterator
+from results_processor import ResultsProcessor
+from regulations_api import RegulationsAPI
+from data_storage import DataStorage
 
 
 class WorkGenerator:
