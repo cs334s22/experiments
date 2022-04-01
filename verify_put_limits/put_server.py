c7ad87f3e1e736e0c60be872042be553021166fe
Matthew Kosack <kosackm@moravian.edu>
2022-04-01 15:26:23 -0400

    made some adjustments to the put_server

--- verify_put_limits/put_server.py
+++ verify_put_limits/put_server.py
@@ -12,11 +12,21 @@ def create_server():
         
     @app.route('/put_data', methods=['PUT'])
     def put_data():
-        files = request.files.getlist('file') # All the files must have 'file'
-        data = json.dumps(request.form.to_dict())
-        print(data)
-        for file in files:
+
+
+        # Check that files are being sent, thus it is an attachment job
+        if request.files.getlist('file') is not None:
+            files = request.files.getlist('file') # All the files must have 'file'
+            data = json.dumps(request.form.to_dict())
+            for file in files:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        # else: this is where the notmal stuff would happen
+
+        # Check for the text file
+        if request.form.getlist('text') is not None:
+            file = request.files.getlist('text')
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
         return 'OK'
 
     return app
