9727baff45d870d1b89e441d8a5f151c36bfd5c3
Mark Morykan <morykanm@moravian.edu>
2022-04-01 15:54:45 -0400

    Write to csv file

--- download_jobs/work_generator.py
+++ download_jobs/work_generator.py
@@ -1,6 +1,7 @@
 import os
 import time
 import dotenv
+import csv
 from search_iterator import SearchIterator
 from results_processor import ResultsProcessor
 from regulations_api import RegulationsAPI
@@ -19,24 +20,24 @@ class WorkGenerator:
         # Gets the timestamp of the last known job in queue
         # last_timestamp = self.job_queue.get_last_timestamp_string(endpoint)
         last_timestamp = '1972-01-01 00:00:00'
+        f = open('missing_jobs.csv', 'w')
+        writer = csv.writer(f)
+        writer.writerow(['job_id', 'job_url', 'job_type'])
         # Finds a job, from the timestamp of the last known job
         # Returns a URL for the specific element
         for result in SearchIterator(self.api, endpoint, last_timestamp):
             if result == {}:
                 continue
-            sum_= 1
             for r in result['data']:
                 if not self.datastorage.exists(r):
-                    print(r['id'], 'number', sum_)
-                else:
-                    print('exists in mongo')
-                sum_ += 1
+                    # print(r['id'])
+                    writer.writerow([r['id'], r['links']['self'], r['type']])
             # If jobs are not in redis
             # add the URL to the jobs_queue (redis server)
             # self.processor.process_results(result)
             # timestamp = result['data'][-1]['attributes']['lastModifiedDate']
             # self.job_queue.set_last_timestamp_string(endpoint, timestamp)
-
+        f.close()
 
 if __name__ == '__main__':
     # I wrapped the code in a function to avoid pylint errors
