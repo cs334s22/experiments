fc1cd1a18824b1e5393df6c2ff6bdc4e09079caf
Matthew Kosack <kosackm@moravian.edu>
2022-04-07 15:35:14 -0400

    encoding and decoding works

--- verify_put_limits/put_client.py
+++ verify_put_limits/put_client.py
@@ -1,42 +1,20 @@
 import requests
-from json import dumps, loads, load
+from json import dumps
 import base64
 
-'''
-This had results, but couldn't work for both json and files
-
-def put_data(data, params, files):
-    requests.put('http://127.0.0.1:8080/put_data', json=dumps(data), params=params, files=files)
-    # requests.put('http://127.0.0.1:8080/put_data', json=dumps(data), params=params)
-    # requests.put('http://127.0.0.1:8080/put_data', params=params, files=files)
-
-if __name__ == '__main__':
-    files = [
-        ('file', open('test_files/test.txt', 'rb')), 
-        ('file', open('test_files/cameraman.tif', 'rb')),
-        ('file', open('test_files/test.pdf', 'rb')),
-        # ('text', open('test_files/test.txt', 'rb')), # This is what a text extracted from a PDF would be
-    ]
-
-    data = {'directory': '/path/to/dir', 
-            'job_id': 17, 
-            'results': 'attachments',
-            }
-    params = {'client_id': 42} 
-    put_data(data, params, files)
-'''
-
-def put_data(data, params, files):
+def put_data(data, params):
     requests.put('http://127.0.0.1:8080/put_data', json=dumps(data), params=params)
+
 
 if __name__ == '__main__':
 
     # The keys should be generated from a combination of the job_id and the file format
     # see download_attachments() from attachments_tests/get_comment_attachments.py for how we get the format
     files = {
-        'test.txt': str(base64.b64encode(open('test_files/test.txt', 'rb').read()).decode('utf-8')),
-        'cameraman.tif': str(base64.b64encode(open('test_files/cameraman.tif', 'rb').read().decode('utf-8'))), 
-        'test.pdf': str(base64.b64encode(open('test_files/test.pdf', 'rb').read().decode('utf-8'))), 
+        'test.txt': base64.b64encode(open('test_files/test.txt', 'rb').read()).decode('ascii'),
+        'cameraman.tif': base64.b64encode(open('test_files/cameraman.tif', 'rb').read()).decode('ascii'),
+        'test.pdf': base64.b64encode(open('test_files/test.pdf', 'rb').read()).decode('ascii'),
+        'waldo.png': base64.b64encode(open('test_files/waldo.png', 'rb').read()).decode('ascii'),
     }
 
     data = {'directory': '/path/to/dir', 
@@ -44,10 +22,4 @@ if __name__ == '__main__':
             'results': files,
             }
     params = {'client_id': 42} 
-    put_data(data, params, files)
-
-
-
-
-
-
+    put_data(data, params)
--- verify_put_limits/put_server.py
+++ verify_put_limits/put_server.py
@@ -2,20 +2,6 @@ from flask import Flask, request, json
 import base64
 import os
 
-'''
-This had results, but couldn't work for both json and files
-
-files = request.files.getlist('file')
-if files is not None:
-    files = request.files.getlist('file') # All the files must have 'file'
-    for file in files:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-data = json.loads(request.get_json()) 
-# This is how we can read the text file
-# if request.form.getlist('text') is not None:
-#     file = request.files.getlist('text')
-#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-'''
 
 def create_server():
     app = Flask(__name__)
@@ -30,7 +16,7 @@ def create_server():
         data = json.loads(request.get_json())
         for key in data['results'].keys():
             with open(os.path.join(app.config['UPLOAD_FOLDER'], key), 'wb') as f:
-                f.write(base64.b64decode(data['results'][key].encode('utf-8')))
+                f.write(base64.b64decode(data['results'][key].encode('ascii')))
 
         return 'OK'
 
